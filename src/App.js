df1a98dac0609dc871632fcefe0b27c23e76c36f
jlampstack <jlampstack@gmail.com>
2021-02-19 11:44:50 -0500

    Passed props up from search

--- src/App.js
+++ src/App.js
@@ -11,26 +11,36 @@ class App extends Component {
 		loading: false,
 	};
 
-	async componentDidMount() {
-		console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
-		console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
-		this.setState({
-			loading: true,
-		});
+	// Nolonger required, search is now handled thru searchUsers()
+	// async componentDidMount() {
+	// 	this.setState({
+	// 		loading: true,
+	// 	});
 
+	// 	const res = await axios.get(
+	// 		`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+	// 	);
+
+	// 	this.setState({ users: res.data, loading: false });
+	// }
+
+	// Search Github users
+	searchUsers = async text => {
+		console.log(text);
+		this.setState({ loading: true });
 		const res = await axios.get(
-			`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
 		);
 
-		this.setState({ users: res.data, loading: false });
-	}
+		this.setState({ users: res.data.items, loading: false });
+	};
 
 	render() {
 		return (
 			<div className='App'>
 				<Navbar title='Github Finder' icon='fab fa-github' />
 				<div className='container'>
-					<Search />
+					<Search searchUsers={this.searchUsers} />
 					<Users loading={this.state.loading} users={this.state.users} />
 				</div>
 			</div>
