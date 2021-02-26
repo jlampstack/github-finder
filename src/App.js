f7a6506b6eeb679b1defece196d84aeadd8c2920
jlampstack <jlampstack@gmail.com>
2021-02-26 10:10:53 -0500

    Added Repos & RepoItem Component

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
 	state = {
 		users: [],
 		user: {},
+		repos: [],
 		loading: false,
 		showClear: false,
 		alert: null,
@@ -40,6 +41,17 @@ class App extends Component {
 		this.setState({ user: res.data, loading: false });
 	};
 
+	// Get users repos
+	getUserRepos = async username => {
+		this.setState({ loading: true });
+
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+		);
+
+		this.setState({ repos: res.data, loading: false });
+	};
+
 	// Clear users from state
 	clearUsers = () => {
 		console.log('clearing state...');
@@ -55,7 +67,7 @@ class App extends Component {
 	};
 
 	render() {
-		const { users, user, loading } = this.state;
+		const { users, user, repos, loading } = this.state;
 
 		return (
 			<Router>
@@ -87,7 +99,9 @@ class App extends Component {
 									<User
 										{...props}
 										getUser={this.getUser}
+										getUserRepos={this.getUserRepos}
 										user={user}
+										repos={repos}
 										loading={loading}
 									/>
 								)}
