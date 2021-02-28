b00ff4f25beafa521d6af43acab0ee1f571b0775
jlampstack <jlampstack@gmail.com>
2021-02-28 16:47:03 -0500

    Added Alert Context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -8,44 +8,38 @@ import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 import './App.css';
 
 const App = () => {
-	// setState
-	const [alert, setAlert] = useState(null);
-
-	// Set Alert
-	const showAlert = (msg, type) => {
-		setAlert({ msg, type });
-		setTimeout(() => setAlert(null), 2000);
-	};
-
 	return (
 		// GithubState is a provider
 		<GithubState>
-			<Router>
-				<div className='App'>
-					<Navbar title='Github Finder' icon='fab fa-github' />
-					<div className='container'>
-						<Alert alert={alert} />
-						<Switch>
-							<Route
-								exact
-								path='/'
-								render={props => (
-									<Fragment>
-										<Search setAlert={showAlert} />
-										<Users />
-									</Fragment>
-								)}
-							/>
-							<Route exact path='/about' component={About} />
-							<Route exact path='/user/:login' component={User} />
-						</Switch>
+			<AlertState>
+				<Router>
+					<div className='App'>
+						<Navbar title='Github Finder' icon='fab fa-github' />
+						<div className='container'>
+							<Alert />
+							<Switch>
+								<Route
+									exact
+									path='/'
+									render={props => (
+										<Fragment>
+											<Search />
+											<Users />
+										</Fragment>
+									)}
+								/>
+								<Route exact path='/about' component={About} />
+								<Route exact path='/user/:login' component={User} />
+							</Switch>
+						</div>
 					</div>
-				</div>
-			</Router>
+				</Router>
+			</AlertState>
 		</GithubState>
 	);
 };
---
+++ src/context/alert/alertReducer.js
@@ -0,0 +1,14 @@
+// Types
+import { SET_ALERT, REMOVE_ALERT } from '../types';
+
+// Reducer
+export default (state, action) => {
+	switch (action.type) {
+		case SET_ALERT:
+			return action.payload;
+		case REMOVE_ALERT:
+			return null;
+		default:
+			return state;
+	}
+};
