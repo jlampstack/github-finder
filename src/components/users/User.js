edf129da5407868ffd03f6435e7767338e7fb9c8
jlampstack <jlampstack@gmail.com>
2021-02-28 11:54:57 -0500

    Moved User State to Context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,15 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+const User = ({ getUserRepos, repos, match }) => {
+	const githubContext = useContext(GithubContext);
+
+	const { getUser, loading, user } = githubContext;
+
 	useEffect(() => {
 		// Belongs to react-rout-dom. Pulls login from URL
 		// console.log(this.props.match);
@@ -98,12 +103,9 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
 		</Fragment>
 	);
 };
-
+// getUserRepos, repos, match
 User.propTypes = {
-	loading: PropTypes.bool,
-	user: PropTypes.object.isRequired,
 	repos: PropTypes.array.isRequired,
-	getUser: PropTypes.func.isRequired,
 	getUserRepos: PropTypes.func.isRequired,
 };
 
