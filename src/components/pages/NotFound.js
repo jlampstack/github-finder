0935a34749e0d59b13dd549cee54b88cbf92a966
jlampstack <jlampstack@gmail.com>
2021-02-28 17:19:26 -0500

    Created Home.js and NotFound.js

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+	return (
+		<div>
+			<h1>Page not found</h1>
+			<div className='lead'>The page you are looking for does not exist.</div>
+		</div>
+	);
+};
+
+export default NotFound;
